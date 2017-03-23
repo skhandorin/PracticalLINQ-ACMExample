906bb11e9c0b4602287d3409e5869bfdf5befd9f
skhandorin <hsatig@mail.ru>
2017-03-23 19:11:35 +0500

    Module 3.4

---
+++ ACM.Library.Test/BuilderTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.Library.Test
+{
+    [TestClass]
+    public class BuilderTest
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void BuildIntegerSequenceTest()
+        {
+            // Arrange
+            Builder listBuilder = new Builder();
+
+            // Act
+            var result = listBuilder.BuildIntergerSequence();
+
+            // Analyze
+            foreach (var item in result)
+            {
+                TestContext.WriteLine(item.ToString());
+            }
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void BuildStringSequenceTest()
+        {
+            // Arrange
+            Builder listBuilder = new Builder();
+
+            // Act
+            var result = listBuilder.BuildStringSequence();
+
+            // Analyze
+            foreach (var item in result)
+            {
+                TestContext.WriteLine(item);
+            }
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void CompareSequenceTest()
+        {
+            // Arrange
+            Builder listBuilder = new Builder();
+
+            // Act
+            var result = listBuilder.CompareSequences();
+
+            // Analyze
+            foreach (var item in result)
+            {
+                TestContext.WriteLine(item.ToString());
+            }
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
